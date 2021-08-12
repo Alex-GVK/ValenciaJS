afbe8c1744001d6b87b5e21f942ff88f62dac864
Alex-GVK <sanikgvk@gmail.com>
2021-08-12 22:53:32 +0300

    create new file js and create slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 $(document).ready(function () {
    $('.commits-slider').slick({
-      infinite: true,
+      centerMode: true,
+      dots: true,
+      centerPadding: '30px',
       slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToScroll: 1,
+      mobileFirst: false,
+      centerMode: true,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '20px',
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '10px',
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: '0px',
+               slidesToShow: 1
+            }
+         }
+
+      ]
+
    });
 });
