8b2f9c1e922297212e5f9347af6c393e2ad35243
Alex-GVK <sanikgvk@gmail.com>
2021-12-22 20:24:57 +0200

    create blocks group_row

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-// Блок запроса
-
 const form = document.querySelector(".form");
 const formTextInput = document.querySelector(".form__text");
 const formButtonInput = document.querySelector(".form__btn");
+// let newsCount = 0;
+// console.log(newsCount);
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const inputValueBtn = formTextInput.value;
-  if (inputValueBtn === '') {
+  if (inputValueBtn === "") {
     alert("Нужно ввести ключевое слово");
-    return
+    return;
   }
 
-   const group = document.querySelector(".group");
+  const group = document.querySelector(".group");
 
   if (group.classList.contains("group--disabled")) {
     group.classList.remove("group--disabled");
   }
 
+  createCirclePreloader();
+
+  const preloader = document.querySelector(".preloader");
+
+
   const apiKey = "7b6944c7dcee48669d9b0f45a147bab1";
 
   const currentDate = new Date();
   const pastDate = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-
-
-// запросы к newsapi
 
   async function fetchNews() {
     const response = await fetch(
@@ -36,8 +38,6 @@ form.addEventListener("submit", function (event) {
     return data;
   }
 
-// Карточка
-
   fetchNews()
     .then((response) => {
       localStorage.setItem("news", JSON.stringify(response));
@@ -47,14 +47,18 @@ form.addEventListener("submit", function (event) {
       newsData = JSON.parse(newsData);
       console.log(newsData);
 
-      let news = response.articles;
+      // let news = response.articles;
 
-      const newsUrl = [];
-      const newsImg = [];
-      const newsPublished = [];
-      const newsTitle = [];
-      const newsDescription = [];
-      const newsSourceName = [];
+      // news.length += newsCount
+
+      const notFoundNews = document.querySelector('.without-info');
+
+      if (news.length === 0) {
+        notFoundNews.classList.remove('without-info--disabled');
+        group.classList.add('group--disabled');
+      } else {
+        notFoundNews.classList.add('without-info--disabled');
+      }
 
       news.forEach((article) => {
         newsUrl.push(article.url);
@@ -78,22 +82,25 @@ form.addEventListener("submit", function (event) {
         imgArticle[i].setAttribute("src", newsImg[i]);
       }
 
-// Creat new format Date in item card
-
       function getDatePublishedAt() {
-         const publishedArticle = document.querySelectorAll(".item-group__date");
+        const publishedArticle = document.querySelectorAll(".item-group__date");
 
-         for (let i = 0; i < publishedArticle.length; i++) {
-            const itemDate = newsPublished[i];
-            const formateItemDate = new Date(itemDate).toLocaleDateString("ru-RU", {year: "numeric", month: "long", day: "numeric",});
-            const prunFormateItemDate = formateItemDate.slice(0, -3)
-            const breakFormateItemDate = prunFormateItemDate.split(" ");
-            const correctDate = prunFormateItemDate.replace(breakFormateItemDate[1], breakFormateItemDate[1] + ",");
-            publishedArticle[i].textContent = correctDate;
-            
-         }
+        for (let i = 0; i < publishedArticle.length; i++) {
+          const itemDate = newsPublished[i];
+          const formateItemDate = new Date(itemDate).toLocaleDateString(
+            "ru-RU",
+            { year: "numeric", month: "long", day: "numeric" }
+          );
+          const prunFormateItemDate = formateItemDate.slice(0, -3);
+          const breakFormateItemDate = prunFormateItemDate.split(" ");
+          const correctDate = prunFormateItemDate.replace(
+            breakFormateItemDate[1],
+            breakFormateItemDate[1] + ","
+          );
+          publishedArticle[i].textContent = correctDate;
+        }
       }
-      getDatePublishedAt()
+      getDatePublishedAt();
 
       const titleArticle = document.querySelectorAll(".item-group__title");
 
@@ -113,10 +120,116 @@ form.addEventListener("submit", function (event) {
         sourceArticle[i].textContent = newsSourceName[i];
       }
     })
-    .catch((error) => {
-      console.log(error);
+    .catch(() => {
+      alert(
+        "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз."
+      );
+    })
+    .finally(() => {
+      preloader.classList.add("preloader--disabled");
     });
 });
+
+function createCirclePreloader() {
+  const header = document.querySelector("header");
+  const preloader = document.createElement("div");
+  preloader.className = "preloader";
+  const circlePreloader = document.createElement("div");
+  circlePreloader.className = "circle-preloader";
+  const textPreloader = document.createElement("p");
+  textPreloader.className = "preloader-text";
+  textPreloader.textContent = "Идет поиск новостей...";
+  preloader.append(circlePreloader, textPreloader);
+  header.after(preloader);
+}
+// createCirclePreloader();
+
+function createPreloaderWithoutInfo() {
+  const header = document.querySelector("header");
+  const withoutInfo = document.createElement("div");
+  withoutInfo.classList.add("without-info", "without-info--disabled");
+  const withoutInfoImage = document.createElement("img");
+  withoutInfoImage.className = "without-info__img";
+  withoutInfoImage.src = "img/not-found.png";
+  const withoutInfoTitle = document.createElement("div");
+  withoutInfoTitle.className = "without-info__title";
+  withoutInfoTitle.textContent = "Ничего не найдено";
+  const withoutInfoText = document.createElement("div");
+  withoutInfoText.className = "without-info__text";
+  withoutInfoText.textContent =
+    "К сожалению по вашему запросу ничего не найдено.";
+  withoutInfo.append(withoutInfoImage, withoutInfoTitle, withoutInfoText);
+  header.after(withoutInfo);
+}
+createPreloaderWithoutInfo();
+
+
+function createNewsCard() {
+  const cardsRow = document.querySelector(".group__row");
+  for (let i = 0; i <= 2; i++) {
+    const cardsColumn = document.createElement("div");
+    cardsColumn.className = "group__column";
+    cardsRow.append(cardsColumn);
+
+    const itemGroupCard = document.createElement("div");
+    itemGroupCard.className = "item-group";
+    cardsColumn.append(itemGroupCard);
+
+    const iconCard = document.createElement("div");
+    iconCard.className = "item-group__icon";
+    const imgCard = document.createElement("img");
+    imgCard.className = "item-group__img";
+    iconCard.append(imgCard);
+
+    const dateCard = document.createElement("div");
+    dateCard.className = "item-group__date";
+
+    const titleCard = document.createElement("div");
+    titleCard.className = "item-group__title";
+
+    const textCard = document.createElement("div");
+    textCard.className = "item-group__text";
+    textCard.textContent = "";
+
+    const websiteCard = document.createElement("div");
+    websiteCard.className = "item-group__website";
+
+    itemGroupCard.append(iconCard, dateCard, titleCard, textCard, websiteCard);
+  }
+}
+createNewsCard();
+
+const newsUrl = [];
+const newsImg = [];
+const newsPublished = [];
+const newsTitle = [];
+const newsDescription = [];
+const newsSourceName = [];
+
+
+
+
+
+
+
+
+const groupBtn = document.querySelector('.group__btn');
+
+groupBtn.addEventListener('click', function() {
+  createNewsCard();
+  const imgArticle = document.querySelectorAll(".item-group__img");
+
+      for (let i = 0; i < imgArticle.length; i++) {
+        imgArticle[i].setAttribute("src", newsImg[i]);
+      }
+
+//       const test = document.querySelectorAll('.item-group');
+// console.log(test.length);
+
+})
+
+
+
 
 $(document).ready(function () {
   $(".commits-slider").slick({
